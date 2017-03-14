dad90d3e92c0b798139e91609bb26475c1a30736
= <lwydctl@gmail.com>
2017-03-14 08:17:04 +0800

    Init Finish Include crashcyltics

--- app/src/main/java/com/example/imetu/imet/BaseActivity.java
+++ app/src/main/java/com/example/imetu/imet/BaseActivity.java
@@ -2,12 +2,19 @@ package com.example.imetu.imet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
     }
+
 }
